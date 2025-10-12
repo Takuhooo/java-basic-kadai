ab2ea7af96619ad4192fa865d30b8fbfa20fae17
Takuhooo <takuho0104@ezweb.ne.jp>
2025-10-12 16:00:44 +0900

    解答に訂正

--- java-basic-kadai/src/kadai_015/Car_Chapter15.java
+++ java-basic-kadai/src/kadai_015/Car_Chapter15.java
@@ -7,22 +7,22 @@ public class Car_Chapter15 {
 	
 	//ギアチェンジして速度を変更するメソッド
 	public void changeGear(int afterGear) {
-		gear = afterGear;
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+		this.gear = afterGear;
 		
 		//ギアに応じて速度をセット
-		switch(gear) {
-		case 1: speed = 10; break;
-		case 2: speed = 20; break;
-		case 3: speed = 30; break;
-		case 4: speed = 40; break;
-		case 5: speed = 50; break;
-		default: speed = 10; break;
+		this.speed = switch(this.gear) {
+		case 1 -> 10;
+		case 2 -> 20;
+		case 3 -> 30;
+		case 4 -> 40;
+		case 5 -> 50;
+		default -> 10;
 		}
 	}
 	
 	//走行速度を表示するメソッド
 	public void run() {
-		System.out.println("ギア" + gear + "に切り替えました");
-		System.out.println("速度は時速" + speed + "kmです");
+		System.out.println("速度は時速" + this.speed + "kmです");
 	}
 }
