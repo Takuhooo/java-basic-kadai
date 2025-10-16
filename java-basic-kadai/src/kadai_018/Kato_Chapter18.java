e14a2c3962acf5de631ba9b1f11c68f02633cd75
Takuhooo <takuho0104@ezweb.ne.jp>
2025-10-16 19:45:50 +0900

    修正

--- java-basic-kadai/src/kadai_018/Kato_Chapter18.java
+++ java-basic-kadai/src/kadai_018/Kato_Chapter18.java
@@ -1,46 +1,40 @@
 package kadai_018;
 
 abstract public class Kato_Chapter18{
-	private String familyName;
-	private String givenName;
-	private String address;
-	
-	//値を取得するメソッド（ゲッター）
-	public String getFamilyName(){
-		return this.familyName;
-	}
-	public String getGivenName(){
-		return this.givenName;
-	}
-	public String getAddress(){
-		return this.address;
-	}
-	
-	//値を設定するメソッド（セッター）
-	public void setFamilyName(String familyName) {
-		this.familyName = familyName;
-	}
-	public void setGivenName(String givenName) {
-		this.givenName = givenName;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
-	//共通の紹介を出力するメソッド（親クラス共通処理）
-	public void commonIntroduce() {
-		System.out.println("名前は" + this.familyName + this.givenName + "です");
-		System.out.println("住所は" + this.address);
-	}
-	
+	public String familyName;
+	public String givenName;
+	public String address;
+
+	// 共通の紹介を出力するメソッド
+    public void commonIntroduce() {
+        System.out.println("名前は" + familyName + givenName + "です");
+        System.out.println("住所は" + address);
+    }
+    
 	// 抽象メソッド（子クラスで必ず実装するメソッド）
 	abstract public void eachIntroduce();
 	
-	//共通紹介＋個別紹介を順に実行するメソッド
+	//紹介を実行するメソッド
 	public void execIntroduce() {
-		commonIntroduce(); //親クラスの共通処理を実行
-		eachIntroduce(); //子クラスごとの実装（オーバーライドされたメソッド）が呼ばれる
-		System.out.println(); //出力の区切り(空行)
+        setFamilyName();     // 姓の設定
+        setGivenName();      // 名の設定（子クラスで実装）
+        setAddress();        // 住所の設定
+        commonIntroduce();   // 共通紹介
+        eachIntroduce();     // 個別紹介
+        System.out.println(); // 改行（区切り）
+	}
+	
+	//性を設定する（共通なので親で実装）
+	public void setFamilyName() {
+		familyName = "加藤";
+	}
+	
+	//名を設定する（子クラスで実装するため中身は空）
+	public void setGivenName(){};
+	
+	//住所を設定する（共通なので親で実装）
+	public void setAddress() {
+		address = "東京都中野区○×です";
 	}
 }
 	
