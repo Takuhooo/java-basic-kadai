6bc4ef1c8291330662e7248372f411f0dcc70943
Takuhooo <takuho0104@ezweb.ne.jp>
2025-10-16 21:47:28 +0900

    修正2回目

--- java-basic-kadai/src/kadai_018/KatoExec_Chapter18.java
+++ java-basic-kadai/src/kadai_018/KatoExec_Chapter18.java
@@ -1,16 +1,21 @@
 package kadai_018;
 
 public class KatoExec_Chapter18 {
-
     public static void main(String[] args) {
-    	
-    	KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
-    	taro.execIntroduce();
-    	
-    	KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
-        ichiro.execIntroduce();
-        
+
+        // --- 各子クラスのインスタンスを作成 ---
+        KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+        KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
         KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+
+        // --- setGivenNameメソッドを実行 ---
+        taro.setGivenName();
+        ichiro.setGivenName();
+        hanako.setGivenName();
+
+        // --- execIntroduceメソッドを実行 ---
+        taro.execIntroduce();
+        ichiro.execIntroduce();
         hanako.execIntroduce();
     }
 }
